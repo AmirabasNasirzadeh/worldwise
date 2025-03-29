be599c5edd83dc07f3a7a23df7cf14f3b1d48df2
Amirabas Nasirzadeh <13.amirabas.nasirzadeh.86@gmail.com>
2025-03-29 14:21:38 +0330

    login and logout feature

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import styles from "./AppLayout.module.css";
 import PageNav from "../components/PageNav";
 import Sidebar from "../components/sidebar";
 import Map from "../components/Map";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
